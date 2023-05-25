814272f1d58cba08b2b50b8abfdced29d1b81496
Nicholas Ng <onenylxus@gmail.com>
2023-05-26 02:36:42 +0800

    Use interval hook

---
+++ src/game/hooks/useInterval.ts
@@ -0,0 +1,23 @@
+// Import
+import { useEffect, useRef } from 'react';
+
+// Use interval hook
+const useInterval = (callback: () => void, delta: number): void => {
+  const callbackRef = useRef(callback);
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    if (delta < 0) {
+      return;
+    }
+
+    const intervalId = setInterval(callbackRef.current, delta);
+    return () => clearInterval(intervalId);
+  }, [delta]);
+};
+
+// Export
+export default useInterval;
